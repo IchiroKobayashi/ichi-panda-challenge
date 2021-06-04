721018e11b20c42f255086a1c1a3ec0ea1b2bfa6
Ichiro Kobayashi <kobayashi@supinf.co.jp>
2021-06-04 16:17:00 +0900

    wip

--- model.py
+++ model.py
@@ -5,6 +5,7 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neural_network import MLPClassifier
+import scipy.stats
 
 """
 ▽学習手法
@@ -16,15 +17,30 @@ from sklearn.neural_network import MLPClassifier
 ・ニューラルネットワーク
 """
 
+ 
+# model_param_set_random = {
+#     LinearRegression(): {},
+#     Lasso(): {},
+#     Ridge(): {},
+#     SVR(): {
+#         "kernel": ["linear", "poly", "rbf", "sigmoid"],
+#         "C": scipy.stats.uniform(0.00001, 1000)
+#     },
+#     RandomForestRegressor(): {
+#         "n_estimators": scipy.stats.randint(10, 300),
+#         "max_depth": scipy.stats.randint(1, 20)
+#     }
+# }
+
 def get_models()->list:
     models = []
     models.append(("LogisticRegression", LogisticRegression(max_iter=5000)))
     models.append(("SVC", SVC()))
-    # models.append(("LinearSVC", LinearSVC(dual=False)))
-    # models.append(("KNeighbors", KNeighborsClassifier()))
-    # models.append(("DecisionTree", DecisionTreeClassifier()))
-    # models.append(("RandomForest", RandomForestClassifier()))
-    # models.append(("MLPClassifier", MLPClassifier(solver='lbfgs', random_state=0, max_iter=5000)))
+    models.append(("LinearSVC", LinearSVC(dual=False)))
+    models.append(("KNeighbors", KNeighborsClassifier()))
+    models.append(("DecisionTree", DecisionTreeClassifier()))
+    models.append(("RandomForest", RandomForestClassifier()))
+    models.append(("MLPClassifier", MLPClassifier(solver='lbfgs', random_state=0, max_iter=10000)))
     return models
 
 if __name__ == "__main__":
--- titanic.py
+++ titanic.py
@@ -3,6 +3,8 @@ import pandas as pd
 import copy
 import model
 from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
+from scipy.stats import randint as sp_randint
 """
 ▽Data Set
 ・PassengerId：データにシーケンシャルでついている番号
@@ -23,18 +25,16 @@ predictors = ["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked"]
 def exec():
     train_raw = pd.read_csv("./seed/titanic_train.csv", dtype={"Age": np.float64}, )
     test_raw  = pd.read_csv("./seed/titanic_test.csv", dtype={"Age": np.float64}, )
-    train_corrected = correct_data(copy.deepcopy(train_raw)) # 参照値、train_rawも変わる
-    test_corrected  = correct_data(copy.deepcopy(test_raw))
+    train_corrected, test_corrected = correct_data(copy.deepcopy(train_raw), copy.deepcopy(test_raw)) # 参照値、train_rawも変わる
     # print(train_raw.head(10)) 
-    # print(train_corrected.head(10)) 
-    train_corr = train_corrected.corr()
+    # train_corr = train_corrected.corr()
     # print(train_corr)
     # 使用するカラムを指定：チケットのクラス、性別、年齢、タイタニック号に乗っていた兄弟と配偶者の数、タイタニック号に乗っていた両親と子どもの数、旅客運賃、乗船場
     results = []
     names = []
     models = model.get_models()
     for name, m in models:
-        # 各モデルを使って交差検証を実施、x:使用するデータセット(カラム指定)、y:予測する値
+        # 各モデルを使って交差検証を実施、x:使用するデータセット(カラム指定)、y:予測する値、cv:○個あるデータブロックのうち、1個を検証用データ、その他を訓練データとして使う
         result = cross_val_score(m, train_corrected[predictors], train_corrected["Survived"], cv=3)
         names.append(name)
         results.append(result)
@@ -59,14 +59,19 @@ def exec():
 
     output_result_as_csv(algo, train_corrected, test_corrected)
 
+def correct_data(train_raw, test_raw):
+    train_raw.Age = train_raw.Age.fillna(test_raw.Age.median()) # テストデータのmedianをとったほうが精度向上する。
+    train_raw.Fare = train_raw.Fare.fillna(test_raw.Fare.median()) # テストデータのmedianをとったほうが精度向上する。
+    test_raw.Age = test_raw.Age.fillna(test_raw.Age.median())
+    test_raw.Fare = test_raw.Fare.fillna(test_raw.Fare.median())
+    train_data = correct_common_data(train_raw)
+    test_data = correct_common_data(test_raw)
+    return train_data, test_data
 
-
-def correct_data(titanic_data):
-    titanic_data.Age = titanic_data.Age.fillna(titanic_data.Age.median())
+def correct_common_data(titanic_data):
     titanic_data.Sex = titanic_data.Sex.replace(['male', 'female'], [0, 1])
     titanic_data.Embarked = titanic_data.Embarked.fillna("S") # これはバイアス。主観入りすぎ
     titanic_data.Embarked = titanic_data.Embarked.replace(['C', 'S', 'Q'], [0, 1, 2])
-    titanic_data.Fare = titanic_data.Fare.fillna(titanic_data.Fare.median())
     return titanic_data
 
 def get_best_model(names, results):
@@ -81,6 +86,18 @@ def get_best_model(names, results):
     return best_model
 
 def output_result_as_csv(algo, train_corrected, test_corrected):
+    # param_grid = {
+    #     "max_depth": [3, None], #distribution
+    #     "n_estimators":[50,100,200,300,400,500],
+    #     "max_features": sp_randint(1, 11),
+    #     "min_samples_split": sp_randint(2, 11),
+    #     "min_samples_leaf": sp_randint(1, 11),
+    #     "bootstrap": [True, False],
+    #     "criterion": ["gini", "entropy"]
+    # }
+    # グリッドサーチによるハイパーパラメータの最適化
+    # gsc = GridSearchCV(algo, parameters, cv=3)
+    # rsc = RandomizedSearchCV(estimator=algo, param_distributions=param_grid, cv=5, n_iter=30) # scoring="accuracy"
     algo.fit(train_corrected[predictors], train_corrected["Survived"])
     predictions = algo.predict(test_corrected[predictors])
     submission = pd.DataFrame({
